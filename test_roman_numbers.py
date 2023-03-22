4291b4008953b3653f22d36bc01327599a712f33
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-22 14:00:53 +0100

    added complex test for 9 = IX

--- test_roman_numbers.py
+++ test_roman_numbers.py
@@ -2,6 +2,14 @@ import pytest
 from roman_numbers import arabic_to_roman
 
 
-def test_arabic_input():
+def test_input():
     with pytest.raises(ValueError):
         arabic_to_roman("arabic_number")
+
+
+def test_arabic_numbers():
+    assert arabic_to_roman(1501) == ("MDI", 0)
+
+
+def test_complex():
+    assert arabic_to_roman(9) == ("IX", 0)
