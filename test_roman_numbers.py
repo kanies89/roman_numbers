c9e88bb59cb29019fa6fd6babecf416f130b4767
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-22 16:50:01 +0100

    roman to arabic test for 9

--- test_roman_numbers.py
+++ test_roman_numbers.py
@@ -3,7 +3,7 @@ from roman_numbers import arabic_to_roman, check_input
 
 
 def test_input():
-    assert check_input("XV") == 1
+    assert check_input("IX") == 9
     with pytest.raises(ValueError):
         arabic_to_roman(4000)
 
